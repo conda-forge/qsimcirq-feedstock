37df5c9a59e81ec85ebec9a42432d68a5c8714e2
Bas Nijholt <bas@nijho.lt>
2023-12-08 11:23:51 -0800

    Fix for missing OpenMP_CXX on MacOS ARM64

---
+++ recipe/build.sh
@@ -0,0 +1,14 @@
+set -ex
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # MacOS specific commands, this only needs to happen for ARM64
+    # but it doesn't hurt to do it for all MacOS builds
+    OPENMP_INCLUDE_DIR=$(echo ${BUILD_PREFIX}/lib/clang/*/include)
+    {
+        echo "set(OpenMP_INCLUDE_DIR \"${OPENMP_INCLUDE_DIR}\")"
+        echo "include_directories(\${OpenMP_INCLUDE_DIR})"
+        cat CMakeLists.txt
+    } > CMakeLists.txt
+fi
+
+python -m pip install . -vvv --no-deps --no-build-isolation
