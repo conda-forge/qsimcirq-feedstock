ccbc418d6de99555ab6579ab2ae3265c3351c37a
Bas Nijholt <bas@nijho.lt>
2023-12-08 15:46:34 -0800

    Add OpenMP_omp_LIBRARY

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # MacOS specific commands, this only needs to happen for ARM64
     # but it doesn't hurt to do it for all MacOS builds
     OPENMP_INCLUDE_DIR=$(echo ${BUILD_PREFIX}/lib/clang/*/include)
+    OpenMP_omp_LIBRARY=$(echo ${BUILD_PREFIX}/lib/libomp.dylib)
     {
         echo "set(OpenMP_INCLUDE_DIR \"${OPENMP_INCLUDE_DIR}\")"
+        echo "set(OpenMP_omp_LIBRARY \"${OpenMP_omp_LIBRARY}\")"
         echo "set(OpenMP_CXX_FLAGS \"-fopenmp=libomp\")"
         echo "set(OpenMP_CXX_LIB_NAMES \"omp\")"
         echo "include_directories(\${OpenMP_INCLUDE_DIR})"
