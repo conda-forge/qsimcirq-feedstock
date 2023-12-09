69ed017cc0f3653767b6e5ecae662ffffc057e19
Bas Nijholt <basnijholt@gmail.com>
2023-12-09 01:38:39 -0800

    Do not set OpenMP paths and flags build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,5 @@
 set -ex
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    # MacOS specific commands, this only needs to happen for ARM64
-    # but it doesn't hurt to do it for all MacOS builds
-    OPENMP_INCLUDE_DIR=$(echo ${BUILD_PREFIX}/lib/clang/*/include)
-    OpenMP_omp_LIBRARY=$(echo ${BUILD_PREFIX}/lib/libomp.dylib)
-    {
-        echo "set(OpenMP_INCLUDE_DIR \"${OPENMP_INCLUDE_DIR}\")"
-        echo "set(OpenMP_omp_LIBRARY \"${OpenMP_omp_LIBRARY}\")"
-        echo "set(OpenMP_CXX_FLAGS \"-fopenmp=libomp\")"
-        echo "set(OpenMP_CXX_LIB_NAMES \"omp\")"
-        echo "include_directories(\${OpenMP_INCLUDE_DIR})"
-        cat CMakeLists.txt
-    } > tmp-CMakeLists.txt
-    mv tmp-CMakeLists.txt CMakeLists.txt
-fi
-
 # Copied from https://github.com/conda-forge/slycot-feedstock/pull/47
 # because $ file ~/micromamba/envs/test5/lib/python3.9/site-packages/qsimcirq/qsim_decide.cpython-39-darwin.so
 # qsim_decide.cpython-39-darwin.so: Mach-O 64-bit bundle x86_64
