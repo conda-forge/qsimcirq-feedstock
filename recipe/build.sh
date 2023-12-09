1244f9cc60b2812278e3836b8fee9b79be20f541
Bas Nijholt <bas@nijho.lt>
2023-12-09 00:47:17 -0800

    Add solution from isuruf for x86_64 in ARM64 builds from https://github.com/conda-forge/slycot-feedstock/pull/47

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,4 +16,20 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     mv tmp-CMakeLists.txt CMakeLists.txt
 fi
 
+# Copied from https://github.com/conda-forge/slycot-feedstock/pull/47
+# because $ file ~/micromamba/envs/test5/lib/python3.9/site-packages/qsimcirq/qsim_decide.cpython-39-darwin.so
+# qsim_decide.cpython-39-darwin.so: Mach-O 64-bit bundle x86_64
+# and: (trimmed error message)
+# ❯ python -c "import qsimcirq"
+# Traceback (most recent call last):
+# ImportError: dlopen(.../qsim_decide.cpython-312-darwin.so, 0x0002): 
+# tried: '.../qsim_decide.cpython-312-darwin.so' (mach-o file, but is an 
+# incompatible architecture (have 'x86_64', need 'arm64'))
+if [ "$target_platform" = "osx-arm64" ]; then
+    export CMAKE_OSX_ARCHITECTURES="arm64"
+    # workaround for https://github.com/scikit-build/scikit-build/issues/589
+    rm -rf $PREFIX/lib/libpython$PY_VER.dylib
+    ln -sf $PREFIX/lib/libc++.dylib $PREFIX/lib/libpython$PY_VER.dylib
+fi
+
 python -m pip install . -vvv --no-deps --no-build-isolation
